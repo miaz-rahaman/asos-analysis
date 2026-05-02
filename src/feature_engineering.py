cd6fcb8d197bf4d2ad3973cd94358835a6005a29
miaz-rahaman <miaz-rahaman>
2026-05-02 09:35:32 +0530

    commit commit 	modified:   src/feature_engineering.py commit

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -2,27 +2,6 @@ import pandas as pd
 
 import re
 
-def get_brand(text):
-    if not isinstance(text, str):
-        return "Unknown"
-
-    match = re.search(r'by ([A-Za-z& ]+)', text)
-    
-    if match:
-        brand = match.group(1).strip()
-
-        # Remove trailing unwanted words (like slogans)
-        brand = re.sub(r'(If|Ready|Throw|Low|In|With|For).*', '', brand).strip()
-
-        return brand
-
-    return "Unknown"
-
-
-def extract_brand(df):
-    df['brand'] = df['description'].apply(get_brand)
-    return df
-
 def clean_brands(df):
     df = df.copy()
 
@@ -45,7 +24,8 @@ def normalize_and_filter_brands(df, min_count=5):
         'Carhartt': 'Carhartt WIP',
         'River': 'River Island',
         'Miss': 'Miss Selfridge',
-        'TopshopWelcome': 'Topshop'
+        'TopshopWelcome': 'Topshop',
+        '& Other Stories': 'Other Stories'
     }
 
 
@@ -76,3 +56,22 @@ def add_stockout_features(df):
     )
 
     return df
+
+KNOWN_BRANDS = [
+    "ASOS", "ASOS DESIGN", "ASOS Curve",
+    "River Island", "Topshop", "Vero Moda",
+    "New Look", "Carhartt WIP",
+    "& Other Stories", "Miss Selfridge",
+    "Native Youth", "4th & Reckless",
+    "AFRM"
+]
+
+def extract_brand_from_list(text):
+    if not isinstance(text, str):
+        return "Unknown"
+
+    for brand in KNOWN_BRANDS:
+        if brand.lower() in text.lower():
+            return brand
+
+    return "Unknown"
